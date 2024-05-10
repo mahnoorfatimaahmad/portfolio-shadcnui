f4f10217444b27dec9682ac3daf3d9c023502357
MahnoorFatimaArshad <158273911+mahnoorfatimaahmad@users.noreply.github.com>
2024-05-10 02:47:46 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import {
+  Header,
+  Hero,
+  About,
+  Skills,
+  Testimonials,
+  Projects,
+  Contact,
+  Footer
+} from "@/components/sections";
+
+import { BackgroundGradientAnimation } from "@/components/bg-gradient";
+
+export default function Home() {
+  return (
+    <div className="overflow-hidden">
+      {/* <BackgroundGradientAnimation> */}
+        <div className="absolute inset-0 z-50 h-full w-full">
+          <div className="flex min-h-[100dvh] flex-col">
+            <Header />
+            <main className="flex-1">
+              <Hero />
+              <Projects />
+              <About />
+              <Skills />
+              <Testimonials />
+              <Contact />
+              <Footer />
+            </main>
+          </div>
+        </div>
+      {/* </BackgroundGradientAnimation> */}
+    </div>
+  );
+}
---
+++ components/sections/index.ts
@@ -0,0 +1,10 @@
+import Header from "./header/header";
+import Hero from "./hero/hero";
+import About from "./about/vertical/About";
+import Skills from "./skills/horizontal/skills";
+import Projects from "./projects/horizontal/projects";
+import Contact from "./contact/contact";
+import Testimonials from "./testimonials/testimonials";
+import Footer from "./footer/footer";
+
+export { Header, Hero, About, Projects, Skills, Testimonials, Contact, Footer };

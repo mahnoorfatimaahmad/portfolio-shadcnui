0fb0f7d7ca71d12b3e16d732d71d284807194c1e
MahnoorFatimaArshad <158273911+mahnoorfatimaahmad@users.noreply.github.com>
2024-05-10 02:59:19 -0700

    Update hero.tsx

--- components/sections/hero/hero.tsx
+++ components/sections/hero/hero.tsx
@@ -8,13 +8,13 @@ function Hero() {
       <div className=" grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
         <div className="space-y-3">
           <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
-            Designer & Developer
+            Welcome to my web
           </div>
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-            Hi, I&apos;m John Doe
+            Hello Everybody! I'm Mahnoor Fatima
           </h1>
           <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-            I design and build beautiful websites. Welcome to my portfolio.
+            Frontend Developer| Web & App Developer | CSS | HTML |TYPESCRIPT |NEXT.JS |REACT.JS
           </p>
         </div>
         <Image
